209242cc8550c5e16d2bdeb191e74b30d7cf0cb6
David Ortet <davidortetsabater@gmail.com>
2024-12-31 10:32:00 +0100

    Test fizzBuzz

--- test/intro.test.js
+++ test/intro.test.js
@@ -1,5 +1,5 @@
-import { describe, test, exp, expect } from "vitest";
-import { max } from "../src/intro";
+import { describe, test, expect } from "vitest";
+import { max, fizzBuzz } from "../src/intro";
 
 describe("max", () => {
   test("should return the first argument if it is greater", () => {
@@ -10,5 +10,20 @@ describe("max", () => {
   });
   test("should return the first argument if arguments are equal", () => {
     expect(max(1, 1)).toBe(1);
+  });
+});
+
+describe("fizzBuzz", () => {
+  test("should return FizzBuzz if the remainder of the division between 5 & 3 is zero ", () => {
+    expect(fizzBuzz(30)).toBe("FizzBuzz");
+  });
+  test("should return Fizz if the remainder of the division between 3 is zero", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+  test("should return Buzz if the remainder of the division between 5 is zero", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+  });
+  test("should return String if the remainder does not give zero in the previous divisions", () => {
+    expect(fizzBuzz(1)).toBe("1");
   });
 });

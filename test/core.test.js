010c254a1142629bda2f531962bed198567e5f48
David Ortet <davidortetsabater@gmail.com>
2025-01-02 09:26:43 +0100

    Test Stack

--- test/core.test.js
+++ test/core.test.js
@@ -15,6 +15,7 @@ import {
   isValidUsername,
   canDrive,
   fetchData,
+  Stack,
 } from "../src/core";
 
 describe("getCoupons", () => {
@@ -167,20 +168,62 @@ describe("fetchData", () => {
   });
 });
 
-describe("test suite", () => {
-  beforeAll(() => {
-    console.log("BeforeAll called");
-  });
+describe("Stack", () => {
+  let stack;
+
   beforeEach(() => {
-    console.log("BeforeEach called");
-  });
-  afterEach(() => {
-    console.log("afterEach called");
-  });
-  afterAll(() => {
-    console.log("afterAll called");
+    stack = new Stack();
   });
 
-  test("test case 1", () => {});
-  test("test case 2", () => {});
+  test("push should add an item to the stack", () => {
+    stack.push(1);
+
+    expect(stack.size()).toBe(1);
+  });
+
+  test("pop should remove and return the top item from the stack", () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.pop()).toBe(2);
+    expect(stack.size()).toBe(1);
+  });
+
+  test("pop should throw an error if stack is empty", () => {
+    expect(() => stack.pop()).toThrow(/empty/i);
+  });
+
+  test("peek should return the top item from stack without removing it", () => {
+    stack.push(1);
+    stack.push(2);
+
+    expect(stack.peek()).toBe(2);
+    expect(stack.size()).toBe(2);
+  });
+
+  test("peek should throw an error if stack is empty", () => {
+    expect(() => stack.peek()).toThrow(/empty/i);
+  });
+
+  test("isEmpty should return true if stack is empty", () => {
+    expect(stack.isEmpty()).toBe(true);
+  });
+  test("isEmpty should return false if stack is not empty", () => {
+    stack.push(1);
+    expect(stack.isEmpty()).toBe(false);
+  });
+
+  test("size should return the number of items in the stack", () => {
+    stack.push(1);
+    expect(stack.size()).toBe(1);
+  });
+
+  test("clear should remove all items from the stack", () => {
+    stack.push(1);
+    stack.push(2);
+
+    stack.clear();
+
+    expect(stack.isEmpty()).toBe(true);
+  });
 });

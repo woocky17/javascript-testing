6f7de14c5bc581ed22fac329146e3fb70cecd305
David Ortet <davidortetsabater@gmail.com>
2025-01-02 12:15:16 +0100

    Spy on Functions

--- test/moocking.test.js
+++ test/moocking.test.js
@@ -2,6 +2,7 @@ import { vi, test, expect, describe } from "vitest";
 import {
   getPriceInCurrency,
   getShippingInfo,
+  login,
   renderPage,
   signUp,
   submitOrder,
@@ -11,6 +12,7 @@ import { getShippingQuote } from "../src/libs/shipping";
 import { trackPageView } from "../src/libs/analytics";
 import { charge } from "../src/libs/payment";
 import { sendEmail } from "../src/libs/email";
+import security from "../src/libs/security";
 
 vi.mock("../src/libs/currency");
 vi.mock("../src/libs/shipping");
@@ -137,5 +139,17 @@ describe("signUp", () => {
     const args = vi.mocked(sendEmail).mock.calls[0];
     expect(args[0]).toBe(email);
     expect(args[1]).toMatch(/welcome/i);
+  });
+});
+
+describe("login", () => {
+  const email = "david@gmail.com";
+
+  test("should email the one-time login code", async () => {
+    const spy = vi.spyOn(security, "generateCode");
+
+    await login(email);
+    const code = spy.mock.results[0].value.toString();
+    expect(sendEmail).toHaveBeenCalledWith(email, code);
   });
 });

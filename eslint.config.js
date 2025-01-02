f61de0149e87f2b79b8fb104d9dd151de88ece32
David Ortet <davidortetsabater@gmail.com>
2025-01-02 13:49:02 +0100

    Running ESLint on TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import eslint from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  { languageOptions: { globals: globals.browser } },
+  eslint.configs.recommended,
+  { ignores: ["dist/"] },
 ];
